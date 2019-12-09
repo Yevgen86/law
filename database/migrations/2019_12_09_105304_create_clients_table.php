ffd2a15d99551284824749672600453babc7f533
hani-mansour <hani.mansour.1990@gmail.com>
2019-12-09 12:21:39 +0100

    added new Client model with a migrate and the relations ship table

---
+++ database/migrations/2019_12_09_105304_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('country')->default('Österreich');
+            $table->string('city');
+            $table->string('plz');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Clients');
+    }
+}
