e78a0ce04d8f4f8cb24db862799d994ba2eca7c3
yevgen <y.kutsy86@gmail.com>
2019-12-16 16:16:30 +0100

    New images for the frontend added Content into static (impressum, leistungen, ueberUns) views inserted Styles coming at the next steps

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,7 @@
 
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div>
-                    <div>Welcome</div>
+    <div class="container-fluid welcome-container">
 
-                    <div>
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
